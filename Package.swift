5860067c44471a59611eb161afca086ebe03f86e
Artur Mkrtchyan <mkrtarturdev@gmail.com>
2020-04-19 22:23:04 +0400

    Updated version

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "Unrealm", targets: ["Unrealm", "UnrealmObjC"])
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-cocoa.git", from: "3.18.0"),
-        .package(url: "https://github.com/wickwirew/Runtime.git", from: "2.1.0")
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: "4.4.1"),
+        .package(url: "https://github.com/arturdev/RuntimeNew.git", from: "2.1.5")
     ],
     targets: [
         .target(
